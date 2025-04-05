2e38b3440d26e9cade6aeb1c8cad83676ae2af3c
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-04-05 15:36:56 +0530

    enabled cors

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { prismaClient } from "@repo/prisma/client";
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.get("/", (req, res) => {
     res.json({
